40079e297124043343f1a68774f88b14f6ea4fc5
Josenito Athayde <josenito@nncs.com.br>
2022-11-26 16:38:53 -0300

    feat:environment and build variables

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { server } from './server/server'
 
-server.listen(3333, () => console.log('Servidor Rodando !'))
+server.listen(process.env.PORT || 3333, () => {
+  console.log(`Servidor Rodando na Porta ${process.env.PORT || 3333} !`)
+})
